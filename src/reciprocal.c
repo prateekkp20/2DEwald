d9cbdc710465ed7faf8707727efefa8c521b6a5e
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2024-06-04 17:49:20 +0530

    Added Uo

--- src/reciprocal.c
+++ src/reciprocal.c
@@ -2,6 +2,12 @@
 #include "const.h"
 #include "fundec.h"
 
+double F_0(double Del_z, double beta){
+    double a = (1-exp(-Del_z*beta))/beta;
+    double b = sqrt(M_PI)*Del_z*erf(beta*Del_z);
+    return a-b;
+}
+
 double F_kl(double *ri, double *rj, double sigma, double psi, double beta, bool same_r){
     if(!same_r){
         double delX=ri[0]-rj[0];
@@ -22,7 +28,9 @@ double F_kl(double *ri, double *rj, double sigma, double psi, double beta, bool 
 }
 
 double reciprocal_n2(double **PosIons, float *ion_charges, int natoms, double betaa, float **box, int K){
-    double reciprocal_energy=0;
+    double reciprocal_energy_i=0;
+    double reciprocal_energy_o=0;
+
     // this is the loop for Ui
     double Length[3]={sqrt(dotProduct(box[0],box[0],3)),sqrt(dotProduct(box[1],box[1],3)),sqrt(dotProduct(box[2],box[2],3))};
     for (int k = -K; k < K+1; k++){
@@ -30,15 +38,26 @@ double reciprocal_n2(double **PosIons, float *ion_charges, int natoms, double be
             if((k==0) && (l==0)) continue;
             double sigma= k/Length[0];
             double psi= l/Length[1];
+            // The double loop sum over natoms in divided into two for loops:
+            // (1) j<i
+            // (2) j==i
             for (int i = 0; i < natoms; i++){
                 for (int j = 0; j < i; j++){
-                    reciprocal_energy+=2*ion_charges[i]*ion_charges[j]*F_kl(PosIons[i],PosIons[j],sigma,psi,betaa,false);
+                    reciprocal_energy_i+=2*ion_charges[i]*ion_charges[j]*F_kl(PosIons[i],PosIons[j],sigma,psi,betaa,false);
                 }
             }
             for (int i = 0; i < natoms; i++){
-                reciprocal_energy+=ion_charges[i]*ion_charges[i]*F_kl(PosIons[i],PosIons[i],sigma,psi,betaa,true);
+                reciprocal_energy_i+=ion_charges[i]*ion_charges[i]*F_kl(PosIons[i],PosIons[i],sigma,psi,betaa,true);
             }
         }
     }
-    return (M_PI/(2*Length[0]*Length[1]))*reciprocal_energy;
+
+    // this is the loop for Uo
+    for (int  i = 0; i < natoms; i++){
+        for (int j = 0; j < natoms; j++){
+            reciprocal_energy_o+=ion_charges[i]*ion_charges[j]*F_0(PosIons[i][2]-PosIons[j][2],betaa);
+        }
+    }
+    
+    return sqrt(M_PI)*reciprocal_energy_o/(Length[0]*Length[1]) +(M_PI/(2*Length[0]*Length[1]))*reciprocal_energy_i;
 }
