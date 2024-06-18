3a5b78772eca17cd47bbe71d811fa7253dd9dee7
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2024-06-18 11:55:39 +0530

    WIP 18 June

--- src/reciprocal_pppm.cpp
+++ src/reciprocal_pppm.cpp
@@ -5,40 +5,15 @@
 #define REAL 0
 #define IMAG 1
 
-double M_n(double u, int n){
-    if(n<2)return 0;
-    else if(n==2){
-        if(u<0 || u>n) return 0;
-        else{
-            return 1-abs(u-1);
-        }
-    }
-    else{
-        if(u<0 || u>n) return 0;
-        else{
-            return (u*M_n(u,n-1)/(n-1))+((n-u)*M_n(u-1,n-1)/(n-1));
-        }
-    }
-}
-
-complex<double>Coeff(double v, double w){
-    const complex<double> t(0, 1);
-    complex<double> bi_mi=exp(t*v*(w-1));
-    complex<double> denox;
-    for (double f = 0; f < w-1; f++){
-        denox+=M_n(f+1,w)*exp(1.0*t*f*v);
-    }
-    if (denox == complex<double>(0, 0)) return 0;
-    bi_mi/=denox;
-    return bi_mi;
-}
-
 double reciprocal_pppm(double **PosIons, float *ion_charges, int natoms, double betaa, float **box, int K, int Grid[], int n[]){
-    double Length[3]={sqrt(dotProduct(box[0],box[0],3)),sqrt(dotProduct(box[1],box[1],3)),sqrt(dotProduct(box[2],box[2],3))};
+    /* Side lengths of the unit cell */
+    omp_set_num_threads(thread::hardware_concurrency());
+    double Length[3]={sqrt(dotProduct(box[0],box[0],3)),sqrt(dotProduct(box[1],box[1],3)),sqrt(dotProduct(box[2],box[2],3))}; 
     double reciprocal_energy_o=0;
     double reciprocal_energy_i=0.0;
-    double G[3][3];
-    double **u,**x_direc, **y_direc, **z_direc;
+    double G[3][3];// Reciprocal Vector
+    double **u; // Fractional coordinates of the atoms in the unit cell
+    double **x_direc, **y_direc, **z_direc;
     u= new double * [natoms];
     x_direc = new double * [natoms];
     y_direc = new double * [natoms];
@@ -49,8 +24,6 @@ double reciprocal_pppm(double **PosIons, float *ion_charges, int natoms, double 
         y_direc[i] = new double  [Grid[1]];
         z_direc[i] = new double  [Grid[2]];
     }
-
-    int l_max=10;
 
     // Volume Calculations
     double A[3];
@@ -72,21 +45,21 @@ double reciprocal_pppm(double **PosIons, float *ion_charges, int natoms, double 
             u[i][j]=Grid[j]*dotProduct(PosIons[i],G[j],3);
         }
     }
-
+    #pragma omp parallel for simd
     // Calculating the cofficients in the x,y and z directions for the Q Matrix
     for (int i = 0; i < natoms; i++){
         // for X direction
         for (int  k1 = 0; k1 < Grid[0]; k1++){
             x_direc[i][k1]=0;
-            for (int  n1 = -l_max; n1 < l_max+1; n1++){
-                x_direc[i][k1]+=M_n(u[i][0]-k1-n1*K,n[0]);
+            for (int  n1 = ceil((u[i][0]-k1-n[0])/Grid[0]); n1 <= floor((u[i][0]-k1)/Grid[0]); n1++){
+                x_direc[i][k1]+=M_n(u[i][0]-k1-n1*Grid[0],n[0]);
             }
         }
         // for Y direction
         for (int  k2 = 0; k2 < Grid[1]; k2++){
             y_direc[i][k2]=0;
-            for (int  n2 = -l_max; n2 < l_max+1; n2++){
-                y_direc[i][k2]+=M_n(u[i][1]-k2-n2*K,n[1]);
+            for (int  n2 = ceil((u[i][1]-k2-n[1])/Grid[1]); n2 <= floor((u[i][1]-k2)/Grid[1]); n2++){
+                y_direc[i][k2]+=M_n(u[i][1]-k2-n2*Grid[1],n[1]);
             }
         }
         // for Z direction
@@ -101,16 +74,17 @@ double reciprocal_pppm(double **PosIons, float *ion_charges, int natoms, double 
     out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) *Grid[0]*Grid[1]*Grid[2]);
     fftw_plan plan;
     plan = fftw_plan_dft_3d(Grid[0],Grid[1],Grid[2], in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-
+    #pragma omp for collapse(3) 
     // initializing the "in" vector with zero values
     for (int tx = 0; tx < Grid[0]; tx++){
         for (int ty = 0; ty < Grid[1]; ty++){
             for (int tz = 0; tz < Grid[2]; tz++){
                 in[tx * (Grid[2] * Grid[1]) + ty * Grid[2] + tz][REAL] = 0.0;
+                // in[tx * (Grid[2] * Grid[1]) + ty * Grid[2] + tz][IMAG] = 0.0;
             }
         }
     }
-
+    #pragma omp for simd
     // Final Q Matrix
     for (int j = 0; j < natoms; j++){
         if (ion_charges[j] == 0)continue;
@@ -136,10 +110,19 @@ double reciprocal_pppm(double **PosIons, float *ion_charges, int natoms, double 
     double TwoPi_Grid[] = {2*M_PI/Grid[0],2*M_PI/Grid[1],2*M_PI/Grid[2]};
     double beta_sq = betaa*betaa;
     double pi_sq = M_PI*M_PI;
+    double PiSq_BetaSq = pi_sq/beta_sq;
+    // #pragma omp parallel for schedule(runtime) reduction(+: reciprocal_energy_i) collapse(3)
+    // #pragma omp parallel for schedule(runtime) reduction(+: reciprocal_energy_i) 
     for (int i = -K; i < K+1; i++){
+    // for (int i = -Grid[0]/2; i < Grid[0]/2+1; i++){
+            double y_part =0;
         for (int j = -K; j< K+1; j++){
-            for (int k = -K; k < K+1; k++){
-                if(i==0 && j==0 && k==0)continue;
+        // for (int j = -Grid[1]/2; j< Grid[1]/2+1; j++){
+            double z_part =0;
+            // for (int k = -K; k < K+1; k++){
+            for (int k = -Grid[2]/2; k < Grid[2]/2+1; k++){
+                // if(i==0 && j==0 && k==0)continue;
+                if(i==0 && j==0)continue;
                 if(i<0) ii=Grid[0]+i;
                 else ii=i;
                 if(j<0) jj=Grid[1]+j;
@@ -148,12 +131,15 @@ double reciprocal_pppm(double **PosIons, float *ion_charges, int natoms, double 
                 else  kk=k;
                 int temp= ii * (Grid[1] * Grid[2]) + jj * Grid[2] + kk;
                 double norm_FQ = out[temp][REAL]*out[temp][REAL]+out[temp][IMAG]*out[temp][IMAG];
-                double factor = pow(i/Length[0],2) + pow(j/Length[1],2) + pow((double)k/Grid[2],2); 
-                reciprocal_energy_i+= norm_FQ * norm(Coeff(TwoPi_Grid[0]*i,n[0])*Coeff(TwoPi_Grid[1]*j,n[1])*Coeff(TwoPi_Grid[2]*k,n[2])) / (factor*exp(pi_sq*factor/beta_sq));
+                double factor = pow(i/Length[0],2) + pow(j/Length[1],2) + pow((double)k/Grid[2],2);
+                // z_part+= norm_FQ * norm(Coeff(TwoPi_Grid[2]*k,n[2])) / (factor*exp(PiSq_BetaSq*factor));
+                reciprocal_energy_i+= norm_FQ * norm(Coeff(TwoPi_Grid[0]*i,n[0])*Coeff(TwoPi_Grid[1]*j,n[1])*Coeff(TwoPi_Grid[2]*k,n[2])) / (factor*exp(PiSq_BetaSq*factor));
             }
+            // reciprocal_energy_i+=z_part*norm(Coeff(TwoPi_Grid[1]*j,n[1]));
         }
+        // reciprocal_energy_i+=y_part*norm(Coeff(TwoPi_Grid[0]*i,n[0]));
     }
-    reciprocal_energy_i*=1/(2*M_PI*Length[0]*Length[1]*Grid[2]);
+    reciprocal_energy_i/=2*M_PI*Length[0]*Length[1]*Grid[2];
     
     // this is the loop for Uo
     for (int  i = 0; i < natoms; i++){
