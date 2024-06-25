38ea847bd0552fb347fc0426d67d8cf675047282
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2024-06-25 15:42:24 +0530

    Changed the functions

--- inc/fundec.h
+++ inc/fundec.h
@@ -1,7 +1,5 @@
 ///declare all functions here
 
-double duration(struct timeb start, struct timeb end);
-
 void print_lammps_input_file(double **PosIons, float *chg, int natoms, float **boxcell, int n_atomtype, int *natoms_type, string *atomtype, int printtrj, int MDstep, char printmode, string filename);
 
 void print_coor(double **PosIons, int natoms, float **boxcell, int n_atomtype, int *natoms_type, string *atomtype, int printtrj, int MDstep,char printmode, string filename);
@@ -14,42 +12,7 @@ void printFor(double **ForceIons, int natoms, string *type);
 
 void printVel(double **Vel, int natoms, string *type);
 
-void printMD(double **PosIons, int natoms, float **boxcell, double **Vel, int MDstep,int nbondatoms, int *batom1, int *batom2, string *bondpot, double **bondpar, int nnonbondatoms, int *nbatom1, int *nbatom2, string *nonbondpot, double **nonbondpar, int **fixatoms, int **pairs, float *mass);
-
-void pairlist(double **PosIons, int natoms, float **boxcell, string *atomtype, int *natoms_type, int n_atomtype, string *type);
-
-void readbondpar(int nbondatoms, int *batom1, int *batom2, string *bondpot, double **bondpar, int **pairs, int natoms);
-
-void readnonbondpar(int nnonbondatoms, int *nbatom1, int *nbatom2, string *nonbondpot, double **nonbondpar, int **pairs, int natoms);
-
-double CalEner(double **PosIons, int natoms, float **boxcell, int nbatoms, int *atom1, int *atom2, string *npot, double **npar);
-
-double CalEner1(double **PosIons, int natoms, float **boxcell, int *batom1, int *batom2, string *bondpot, int *nbatom1, int *nbatom2, string *nonbondpot, double **bondpar, double **nonbondpar, int **pairs, int randatom, int intdoub, int randatom2);
-
-void CalFor(double **PosIons, double **ForIons,  int natoms, float **boxcell, int nbatoms, int *atom1, int *atom2, string *npot, double **npar);
-
-double CalEnerFor(double **PosIons, int natoms, float **boxcell, double **ForceIons, string filename, bool bonded, bool forces);
-
-double CalKinEner(double **Vel, int natoms, float *mass);
-
-float CalTemp(double KEner, int natoms);
-
-void res_vel(double **vel, int natoms, float Temp, float *mass);
-
-void wrap_coor(double **PosIons, int natoms, float **boxcell);
-
-void montecarlo(double **PosIons, int natoms, float **boxcell, float *mass, string *type, int n_atomtype, int *natoms_type, string *atomtype,float Temp, int nbondatoms, int *batom1, int *batom2, string *bondpot, double **bondpar, int nnonbondatoms, int *nbatom1, int *nbatom2, string *nonbondpot, double **nonbondpar, int **fixatoms, int **pairs);
-
-void minimization(double **PosIons, int natoms, double **ForceIons, float **boxcell, double Pot, string *type, int **fixatoms);
-
-
-pair <double, float>  first_wolfe_brac(double **PosIons, double **PrevPos,double **ForceIons, int natoms,  float **boxcell, double Pot, float c1_wolfe, float alpha, float brac_par, int **fixatoms);
-
-void md(double **PosIons, int natoms, float **boxcell, double **vel, double **ForceIons, float *mass, float dt, float Temp, int nbondatoms, int *batom1, int *batom2, string *bondpot, double **bondpar, int nnonbondatoms, int *nbatom1, int *nbatom2, string *nonbondpot, double **nonbondpar, int **fixatoms);
-
-void simuAnn(double **PosIons, int natoms, float **boxcell, double **vel, double **ForceIons, float *mass, float dt, float TempB, float TempE, int nbondatoms, int *batom1, int *batom2, string *bondpot, double **bondpar, int nnonbondatoms, int *nbatom1, int *nbatom2, string *nonbondpot, double **nonbondpar, int **fixatoms, int rampsize, int rampstep);
-
-void CalDens(double **PosIons, int natoms, float **boxcell, int n_atomtype, int *natoms_type, double lim1, double dx, double *density);
+void printprobVel(double **vel, int natoms, float *mass, float Temp);
 
 double self(int n_atomtype, int *natoms_type, float *chargs, float betaa);
 
@@ -57,7 +20,7 @@ double real(double **PosIons, float *ion_charges, int natoms, double betaa, floa
 
 double F_0(double Del_z, double beta);
 
-double F_kl(double *ri, double *rj, double sigma, double psi, double beta, bool same_r);
+double F_kl(double *ri, double *rj, double sigma, double psi, double beta, bool same_r, float **box);
 
 double reciprocal_n2(double **PosIons, float *ion_charges, int natoms, double betaa, float **box, int K);
 
@@ -68,16 +31,20 @@ double integrand_reciprocal(double h, void *params);
 double dist(double **PosIons, int atom1, int atom2, float **box);
 
 template<typename T>
-double dotProduct(T v1, T v2, size_t size) ;
+double dotProduct(T v1, T v2, size_t n) ;
 
 template<typename T1>
 void crossProduct(T1 v_A, T1 v_B, double *out);
 
-long double error(long double a, long double b);
-
 double M_n(double u, int n);
 
 complex<double>Coeff(double v, double w);
+
+template<typename T2>
+double error(T2 a, T2 b);
+
+template<typename T3>
+double percentReduction(T3 newValue, T3 oldValue);
 
 double reciprocal_fft_integrand(double h, void *params);
 
