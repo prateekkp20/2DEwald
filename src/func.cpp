9f1db7f2b455c045d8d6b352490aedc7ab21c8b3
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2024-06-25 15:43:19 +0530

    Added New Functions

--- src/func.cpp
+++ src/func.cpp
@@ -2,23 +2,89 @@
 #include "const.h"
 #include "fundec.h"
 
+// Function to calculate the dot product of two vectors of size n
 template<typename T>
-double dotProduct(T v1, T v2, size_t size){
+double dotProduct(T v1, T v2, size_t n){
     double result = 0;
-    for (size_t i = 0; i < size; ++i) {
+    for (size_t i = 0; i < n; ++i) {
         result += v1[i] * v2[i];
     }
     return result;
 }
-
 template double dotProduct<float*>(float* v1, float* v2, size_t size);
 template double dotProduct<double*>(double* v1, double* v2, size_t size);
 
+// Function to calculate the cross product of two vectors and store it in the "out" vector
 template<typename T1>
 void crossProduct(T1 v_A, T1 v_B, double *out){
    out[0] = v_A[1] * v_B[2] - v_A[2] * v_B[1];
    out[1] = -(v_A[0] * v_B[2] - v_A[2] * v_B[0]);
    out[2] = v_A[0] * v_B[1] - v_A[1] * v_B[0];
 }
-
 template void crossProduct<float*>(float*, float*, double*);
+
+double F_0(double Del_z, double beta){
+    double a = (1-exp(-Del_z*Del_z*beta*beta))/beta;
+    double b = sqrt(M_PI)*Del_z*erf(beta*Del_z);
+    return a-b;
+}
+
+double F_kl(double *ri, double *rj, double sigma, double psi, double beta, bool same_r, float **box){
+    if(!same_r){
+        double delX=ri[0]-rj[0];
+        double delY=ri[1]-rj[1];
+        double delZ=ri[2]-rj[2];
+        sigma*=2*M_PI;
+        psi*=2*M_PI;
+        double norm_sigma_psi=sqrt(sigma*sigma+psi*psi);
+        double a = cos(sigma*delX+psi*delY)/norm_sigma_psi;
+        double b = exp(delZ*norm_sigma_psi)*erfc(beta*delZ+(norm_sigma_psi/(2*beta)))+exp(-delZ*norm_sigma_psi)*erfc(-beta*delZ+(norm_sigma_psi/(2*beta)));
+        return a*b;
+    }
+    else{
+        double norm_sigma_psi=2*M_PI*sqrt(sigma*sigma+psi*psi);
+        return (2/norm_sigma_psi)*erfc(norm_sigma_psi/(2*beta));
+    }
+    return 0;
+}
+
+double M_n(double u, int n){
+    if(n<2)return 0;
+    else if(n==2){
+        if(u<0 || u>n) return 0;
+        else{
+            return 1-abs(u-1);
+        }
+    }
+    else{
+        if(u<0 || u>n) return 0;
+        else{
+            return (u*M_n(u,n-1)/(n-1))+((n-u)*M_n(u-1,n-1)/(n-1));
+        }
+    }
+}
+
+complex<double>Coeff(double v, double w){
+    const complex<double> t(0, 1);
+    complex<double> bi_mi=exp(t*v*(w-1));
+    complex<double> denox;
+    for (double f = 0; f < w-1; f++){
+        denox+=M_n(f+1,w)*exp(t*f*v);
+    }
+    if (denox == complex<double>(0, 0)) return 0;
+    bi_mi/=denox;
+    return bi_mi;
+}
+
+template<typename T2>
+double error(T2 a, T2 b){
+    return abs(a-b);
+}
+template double error<double>(double, double);
+
+template<typename T3>
+double percentReduction(T3 newValue, T3 oldValue){
+    return 100*abs(newValue-oldValue)/oldValue;
+}
+template double percentReduction<double>(double, double);
+
