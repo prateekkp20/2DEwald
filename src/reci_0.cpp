8cf0ea4e103ad208d92e3a3e0f5b820edd179be7
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2024-08-12 11:53:52 +0530

    Reciprocal Energy (k==0)

---
+++ src/reci_0.cpp
@@ -0,0 +1,23 @@
+#include "libinclude.h"
+#include "const.h"
+#include "fundec.h"
+
+// #define ENABLE_OMP 1
+
+double reci0(double **PosIons, float *ion_charges, int natoms, double betaa, float **box){
+    double energy = 0;
+    double Length[3]={sqrt(dotProduct(box[0],box[0],3)),sqrt(dotProduct(box[1],box[1],3)),sqrt(dotProduct(box[2],box[2],3))};
+
+    #if defined ENABLE_OMP
+        omp_set_num_threads(thread::hardware_concurrency());
+        #pragma omp parallel for simd schedule(runtime) reduction(+: energy)
+    #endif
+    
+    for (int  i = 0; i < natoms; i++){
+        for (int j = 0; j < i; j++){
+            energy+=ion_charges[i]*ion_charges[j]*F_0(PosIons[i][2]-PosIons[j][2],betaa);
+        }
+    }
+    
+    return 2*sqrt(M_PI)*energy/(Length[0]*Length[1]);;
+}
