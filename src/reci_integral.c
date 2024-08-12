62507779b7e05dc67ddbe288ae1a747cdb5623c1
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2024-08-12 12:18:11 +0530

    removed k==0

--- src/reci_integral.c
+++ src/reci_integral.c
@@ -1,11 +1,11 @@
-/*This File Calculates the reciprocal energy using the integral methos developed by Kawata et. al*/
+/*This File Calculates the reciprocal energy (k!=0) using the integral method developed by Kawata et. al*/
 #include "libinclude.h"
 #include "const.h"
 #include "fundec.h"
 
-#define ENABLE_OMP 1
+// #define ENABLE_OMP 1
 
-struct reciprocal_n_params {
+struct reciprocal_n_params{
   double** PosIons;
   float* ion_charges;
   int natoms;
@@ -68,16 +68,5 @@ double reciprocal_kawata(double **PosIons, float *ion_charges, int natoms, doubl
     double Length[3]={sqrt(dotProduct(box[0],box[0],3)),sqrt(dotProduct(box[1],box[1],3)),sqrt(dotProduct(box[2],box[2],3))};
     double reciprocal_energy_o=0;
 
-    // this is the loop for Uo
-    #if defined ENABLE_OMP
-        omp_set_num_threads(thread::hardware_concurrency());
-        #pragma omp parallel for simd schedule(runtime) reduction(+: reciprocal_energy_o)
-    #endif
-    
-    for (int  i = 0; i < natoms; i++){
-        for (int j = 0; j < i; j++){
-            reciprocal_energy_o+=ion_charges[i]*ion_charges[j]*F_0(PosIons[i][2]-PosIons[j][2],betaa);
-        }
-    }
-    return 2*sqrt(M_PI)*reciprocal_energy_o/(Length[0]*Length[1])+result;
+    return result;
 }
