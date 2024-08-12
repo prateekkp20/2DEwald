0f3e63d50801dc8444035c5ff013659da0434ca3
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2024-08-12 12:12:26 +0530

    removed if

--- src/real.c
+++ src/real.c
@@ -14,11 +14,9 @@ double real(double **PosIons, float *ion_charges, int natoms, double betaa, floa
 
         for (int i = 0; i < natoms; i++){
             for (int j = 0; j < i; j++){
-                if(i!=j){
                     double modR=dist(PosIons,i,j,box);
                     if(modR>cutoff)continue;
                     real_energy+=(ion_charges[i]*ion_charges[j]*erfc(betaa*modR))/modR;
-                }
             }
         }
     
