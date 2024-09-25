027cb3cff54c05dc51d22ea94545e437835cfb41
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2024-09-25 16:37:39 +0530

    programs optimizations

--- src/reci_integral.c
+++ src/reci_integral.c
@@ -3,7 +3,7 @@
 #include "const.h"
 #include "fundec.h"
 
-// #define ENABLE_OMP 1
+#define ENABLE_OMP 1
 
 struct reciprocal_n_params{
   double** PosIons;
@@ -12,6 +12,7 @@ struct reciprocal_n_params{
   double betaa;
   double** box;
   int K;
+  double* Length;
 };
 
 double integrand_reciprocal(double h, void *params){
@@ -23,11 +24,11 @@ double integrand_reciprocal(double h, void *params){
     double betaa = p->betaa;
     double **box = p->box;
     int K = p->K;
+    double* Length = p->Length;
 
     double reciprocal_energy_i=0;
-    //Length of the sides of the unit cell
-    double Length[3]={sqrt(dotProduct(box[0],box[0],3)),sqrt(dotProduct(box[1],box[1],3)),sqrt(dotProduct(box[2],box[2],3))};
 
+    complex<double> t(0,1);
     #if defined ENABLE_OMP
         #pragma omp parallel for simd schedule(runtime) reduction(+: reciprocal_energy_i) collapse(2)
     #endif
@@ -36,7 +37,6 @@ double integrand_reciprocal(double h, void *params){
         for (int l = -K; l < K+1; l++){
             if((k==0) && (l==0))continue;
             complex<double> s_kh=0;
-            complex<double> t(0,1);
             double G[3]={2*M_PI*k/Length[0], 2*M_PI*l/Length[1], h};
             for (int  i = 0; i < natoms; i++){
                 double G_dot_r=G[0]*PosIons[i][0]+G[1]*PosIons[i][1]+G[2]*PosIons[i][2];
@@ -48,25 +48,23 @@ double integrand_reciprocal(double h, void *params){
             reciprocal_energy_i+=norm_sg/(norm_g*exp(norm_g/(4*betaa*betaa)));
         }
     }
-    reciprocal_energy_i*=1/(Length[0]*Length[1]);
     return reciprocal_energy_i;
 }
 
 double reciprocal_kawata(double **PosIons, double *ion_charges, int natoms, double betaa, double **box, int K) {
     // this is for Ui
+    //Length of the sides of the unit cell
+    double Length[3]={sqrt(dotProduct(box[0],box[0],3)),sqrt(dotProduct(box[1],box[1],3)),sqrt(dotProduct(box[2],box[2],3))};
     gsl_integration_workspace *workspace = gsl_integration_workspace_alloc(100);
 
     gsl_function F;
     F.function = &integrand_reciprocal; // Set the function to integrate
-    reciprocal_n_params params = {PosIons, ion_charges, natoms, betaa, box, K};
+    reciprocal_n_params params = {PosIons, ion_charges, natoms, betaa, box, K, Length};
     F.params = &params;
 
     double result, error;
     gsl_integration_qagi(&F, 1e-8, 1e-2, 100, workspace, &result, &error);
     gsl_integration_workspace_free(workspace); // Free workspace memory
-
-    double Length[3]={sqrt(dotProduct(box[0],box[0],3)),sqrt(dotProduct(box[1],box[1],3)),sqrt(dotProduct(box[2],box[2],3))};
-    double reciprocal_energy_o=0;
-
+    result*=1/(Length[0]*Length[1]);
     return result;
 }
