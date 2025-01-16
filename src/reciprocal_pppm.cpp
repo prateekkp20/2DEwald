3734d4251a860ab63a626b35f14654a124d99357
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2025-01-16 15:45:10 +0530

    deletion

--- src/reciprocal_pppm.cpp
+++ src/reciprocal_pppm.cpp
@@ -9,7 +9,6 @@ double reciprocal_pppm(double **PosIons, double *ion_charges, int natoms, double
     /* Side lengths of the unit cell */
     omp_set_num_threads(thread::hardware_concurrency());
     double Length[3]={sqrt(dotProduct(box[0],box[0],3)),sqrt(dotProduct(box[1],box[1],3)),sqrt(dotProduct(box[2],box[2],3))}; 
-    double reciprocal_energy_o=0;
     double reciprocal_energy_i=0.0;
     double G[3][3];// Reciprocal Vector
     double **u; // Fractional coordinates of the atoms in the unit cell
@@ -141,12 +140,5 @@ double reciprocal_pppm(double **PosIons, double *ion_charges, int natoms, double
     }
     reciprocal_energy_i/=2*M_PI*Length[0]*Length[1]*Grid[2];
     
-    // this is the loop for Uo
-    // #pragma omp parallel for simd schedule(runtime) reduction(+: reciprocal_energy_o)
-    // for (int  i = 0; i < natoms; i++){
-    //     for (int j = 0; j < i; j++){
-    //         reciprocal_energy_o+=ion_charges[i]*ion_charges[j]*F_0(PosIons[i][2]-PosIons[j][2],betaa);
-    //     }
-    // }
-    return 2*sqrt(M_PI)*reciprocal_energy_o/(Length[0]*Length[1])+reciprocal_energy_i;
+    return reciprocal_energy_i;
 }
