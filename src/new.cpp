535ce67d849a322739d714cd5394f223d0ee88ee
Prateek Kumar Pandey <prateekkp20@iitk.ac.in>
2025-02-12 14:37:06 +0530

    correction factor functions

--- src/new.cpp
+++ src/new.cpp
@@ -5,15 +5,16 @@
 #include "header.h"
 
 struct C_params{
-    double t; double L; double gamm; double n;
+    double t; double L; double gamma; double n;
 };
 
 struct constant_params{
     int kx; int ky; int kz; double lx; double ly; double lz; double gamma;
 };
 
-double hat_function(double s, double gamm, double L){
-    double result = 1/(1+exp(-gamm*(0.5*L+s))) + 1/(1+exp(-gamm*(0.5*L-s))) - 1;
+double hat_function(double s, double gamma, double L){
+    double result = 1/(1+exp(-gamma*(0.5*L+s))) + 1/(1+exp(-gamma*(0.5*L-s))) - 1; // sigmoid type function
+    // double result = tanh(gamma*(s+0.5*L)) - tanh(gamma*(s-0.5*L));
     return result;
 }
 
@@ -21,22 +22,22 @@ double C_integrand(double s, void *params){
     C_params* p = (struct C_params*)params;
     double t = p->t;
     double L = p->L;
-    double gamm = p->gamm;
+    double gamma = p->gamma;
     double n = p->n;
 
-    double result=exp(-s*t*s*t)*cos(2*M_PI*n*s/L)*hat_function(s,gamm,L);
+    double result=exp(-s*t*s*t)*cos(2*M_PI*n*s/L)*hat_function(s,gamma,L);
     return result;
 }
 
-double C(double t, double n, double gamm, double L){
+double C(double t, double n, double gamma, double L){
 
     gsl_integration_workspace *workspace = gsl_integration_workspace_alloc(200);
     gsl_function F;
     F.function = &C_integrand; // Set the function to integrate
-    C_params params = {t, L, gamm, n};
+    C_params params = {t, L, gamma, n};
     F.params = &params;
     double result, error;
-    gsl_integration_qagi(&F, 1e-4, 1e-2, 200, workspace, &result, &error);
+    gsl_integration_qagi(&F, 1e-8, 1e-6, 200, workspace, &result, &error);
     gsl_integration_workspace_free(workspace); // Free workspace memory
 
     return result/L;
@@ -45,7 +46,11 @@ double C(double t, double n, double gamm, double L){
 double constant_integrand(double t, void *params){
     constant_params* p = (struct constant_params*)params;
     int kx = p->kx; int ky = p->ky; int kz = p->kz; double lx = p->lx; double ly = p->ly; double lz = p->lz; double gamma = p->gamma;
-    double g = M_PI*M_PI*((kx*kx)/(lx*lx)+(ky*ky)/(ly*ly));
+    double m[3];
+    for (int t = 0; t < 3; t++){
+        m[t]=kx*G[0][t]+ky*G[1][t];   
+    }
+    double g=M_PI*M_PI*dotProduct(m,m,3);
     double result = (1/(t*t))*C(t,kz,gamma,lz)*exp(-g/(t*t));
     return result;
 }
@@ -59,21 +64,20 @@ double constantterm(int kx, int ky, int kz, double lx, double ly, double lz, dou
     F.params = &params;
     double result, error;
     size_t neval;
-    // gsl_integration_qng(&F, 0, beta, 1e-4, 1e-2, &result, &error, &neval);
+    gsl_integration_qng(&F, 0, beta, 1e-8, 1e-7, &result, &error, &neval);
     // int gsl_integration_qng(const gsl_function *f, double a, double b, double epsabs, double epsrel, double *result, double *abserr, size_t *neval)
 
-    gsl_integration_qag(&F,0, beta, 1e-4, 1e-2, 200, 3, workspace, &result, &error);// this has less error 
+    // gsl_integration_qag(&F,0, beta, 1e-4, 1e-2, 200, 3, workspace, &result, &error);// this has less error 
     // int gsl_integration_qag(const gsl_function *f, double a, double b, double epsabs, double epsrel, size_t limit, int key, gsl_integration_workspace *workspace, double *result, double *abserr)
 
-    cout<<"Error: "<<error<<endl;
     gsl_integration_workspace_free(workspace); // Free workspace memory
 
     return result;
 }
 
-int main(){
-    cout<<fixed<<setprecision(15)<<C(20,3,400,1)<<endl;
-    cout<<fixed<<setprecision(15)<<constantterm(2,2,2,10,10,10,5.42/10,400)<<endl;
-    cout<<"FILE RUNS"<<endl;
-    return 0;
-}
+// int main(){
+//     cout<<fixed<<setprecision(15)<<C(20,3,400,1)<<endl;
+//     cout<<fixed<<setprecision(15)<<constantterm(2,2,2,10,10,10,5.42/10,400)<<endl;
+//     cout<<"FILE RUNS"<<endl;
+//     return 0;
+// }
